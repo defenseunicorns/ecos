866e9676f3863cc92ecbfe843ded20af7335741b
Mike Vanhemert <mike.vanhemert@defenseunicorns.com>
2023-07-26 21:48:29 -0600

    Add ecos -> zarf example

---
+++ examples/zarf-config/ecos/facts/facts.tf
@@ -0,0 +1,15 @@
+output "axolotl_habitat" {
+  value = "Lake Xochimilco, Mexico City, Mexico"
+}
+
+output "axolotl_etymology" {
+  value = "Xolotl was the Aztec god of fire, lightning, deformities, and death; dog-head; slipperty or wrinkled one, servant, slave"
+}
+
+output "axolotl_breeding" {
+  value = "Dance"
+}
+
+output "axolotl_flavor" {
+  value = "Eels"
+}
